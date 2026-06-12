3a0fb40f016579be6850093687818a5c34631d40
ashikurrahmanbhuiyan <s2010376107@ru.ac.bd>
2026-06-12 22:56:32 +0600

    init to learn test

---
+++ src/test/java/com/learn/springTest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
